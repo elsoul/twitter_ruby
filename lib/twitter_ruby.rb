b355db077af4c0ea1373f25ba8c8c79e7e2f40e2
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2020-11-15 23:14:42 +0100

    add update

--- lib/twitter_ruby.rb
+++ lib/twitter_ruby.rb
@@ -4,38 +4,37 @@ require "twitter"
 module TwitterRuby
   class Error < StandardError; end
   class << self
-  attr_accessor :configuration
-  end
-  class TwitterClient
-
-    def initialize
-      @tw = Twitter::REST::Client.new do |config|
-        config.consumer_key        = TwitterRuby.configure.consumer_key
-        config.consumer_secret     = TwitterRuby.configure.consumer_secret
-        config.access_token        = TwitterRuby.configure.access_token
-        config.access_token_secret = TwitterRuby.configure.access_token_secret
-      end
-    end
+    attr_accessor :configuration
 
     def update tweet: "Test Tweet from `twitter_ruby` gem!"
-      { status: true, response: "Posted : #{tweet}" } if @tw.update tweet
+      tw(access_token: "", access_token_secret: "").update tweet ? "Posted : #{tweet}" : "Something Wrong..."
     rescue StandardError => error
-      { status: false, response: "Error : #{error}" }
+      error
     end
-  end
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration)
+
+    def tw access_token: "token", access_token_secret: "secret"
+      Twitter::REST::Client.new do |config|
+        config.consumer_key        = TwitterRuby.configuration.consumer_key
+        config.consumer_secret     = TwitterRuby.configuration.consumer_secret
+        config.access_token        = access_token
+        config.access_token_secret = access_token_secret
+      end
+    rescue StandardError => error
+      error
+    end
+
+    def self.configure
+      self.configuration ||= Configuration.new
+      yield(configuration)
+    end
   end
 
   class Configuration
-    attr_accessor :consumer_key, :consumer_secret, :access_token, :access_token_secret
+    attr_accessor :consumer_key, :consumer_secret
 
     def initialize
       @consumer_key = nil
       @consumer_secret = nil
-      @access_token = nil
-      @access_token_secret = nil
     end
   end
 end
